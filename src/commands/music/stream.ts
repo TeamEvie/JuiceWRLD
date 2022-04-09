e4e68b5e7a4b1ee9a44fd7b9b83a43cfd289afa0
Tristan Camejo <twisttaan@icloud.com>
2022-04-09 18:34:53 +1000

    feat: temp shutdown

--- src/commands/music/stream.ts
+++ src/commands/music/stream.ts
@@ -1,79 +1,115 @@
-import { ReplyStatusEmbed, StatusEmoji } from "#root/classes/EvieEmbed";
+import {
+	EvieEmbed,
+	ReplyStatusEmbed,
+	StatusEmoji,
+} from "#root/classes/EvieEmbed";
 import { registeredGuilds } from "#utils/parsers/envUtils";
 import {
-  ApplicationCommandRegistry,
-  Command,
-  RegisterBehavior,
+	ApplicationCommandRegistry,
+	Command,
+	RegisterBehavior,
 } from "@sapphire/framework";
-import type { AutocompleteInteraction, CommandInteraction } from "discord.js";
+import {
+	AutocompleteInteraction,
+	CommandInteraction,
+	MessageActionRow,
+	MessageButton,
+} from "discord.js";
 
 export class Stream extends Command {
-  public override async chatInputRun(
-    interaction: CommandInteraction
-  ): Promise<void> {
-    if (!interaction.inCachedGuild()) return;
-    const station = interaction.options.getString("station");
+	public override async chatInputRun(
+		interaction: CommandInteraction
+	): Promise<void> {
+		if (!interaction.inCachedGuild()) return;
+		const station = interaction.options.getString("station");
 
-    if (!station) {
-      await ReplyStatusEmbed(
-        StatusEmoji.FAIL,
-        "No station specified.",
-        interaction,
-        true
-      );
-      return;
-    }
+		if (!station) {
+			await ReplyStatusEmbed(
+				StatusEmoji.FAIL,
+				"No station specified.",
+				interaction,
+				true
+			);
+			return;
+		}
 
-    const status = await interaction.client.radio.subscribeToGenre(
-      station,
-      interaction.member
-    );
-    await ReplyStatusEmbed(
-      status.error ? StatusEmoji.FAIL : StatusEmoji.SUCCESS,
-      status.message,
-      interaction
-    );
-  }
+		interaction.reply({
+			embeds: [
+				new EvieEmbed().setTitle("Info").setDescription(
+					`Juice WRLD is currently shut down, as we thought unreleased music would not have any copyright issues.
+            After more research, we have come to the conclusion that we will be contacting [Grade A](https://gradeaproduction.com/),
+            before streaming anymore music. We're sorry for the inconvenience, we recommend for the moment to use [Activities](https://discord.com/oauth2/authorize?client_id=819778342818414632&scope=bot%20applications.commands)
+            a bot that can allow you to use YouTube Together to watch YouTube at the same time with others in a voice chat.`
+				),
+			],
+			components: [
+				new MessageActionRow().addComponents(
+					new MessageButton()
+						.setURL("https://discord.gg/5MWQeVgP7f")
+						.setLabel("Support Server")
+						.setStyle("LINK")
+				),
+			],
+		});
 
-  public override async autocompleteRun(interaction: AutocompleteInteraction) {
-    if (!interaction.guild) return;
-    const stationData = interaction.client.radio.radios;
-    const query = interaction.options.getString("station") ?? "";
+		// const status = await interaction.client.radio.subscribeToGenre(
+		//   station,
+		//   interaction.member
+		// );
+		// await ReplyStatusEmbed(
+		//   status.error ? StatusEmoji.FAIL : StatusEmoji.SUCCESS,
+		//   status.message,
+		//   interaction
+		// );
+	}
 
-    // Collection<string, AudioPlayer>();
-    // filter the stations by the query, the station name is the string key
+	public override async autocompleteRun(
+		interaction: AutocompleteInteraction
+	) {
+		if (!interaction.guild) return;
+		const stationData = interaction.client.radio.radios;
+		const query = interaction.options.getString("station") ?? "";
 
-    return await interaction.respond(
-      stationData.map((value, key) => {
-        return {
-          name: `📻 ${key}`,
-          value: key,
-        };
-      })
-    );
-  }
+		// Collection<string, AudioPlayer>();
+		// filter the stations by the query, the station name is the string key
 
-  public override registerApplicationCommands(
-    registry: ApplicationCommandRegistry
-  ) {
-    registry.registerChatInputCommand(
-      {
-        name: this.name,
-        description: "Tune-in to a Juice WRLD radio station.",
-        options: [
-          {
-            name: "station",
-            description: "The station to tune-in to.",
-            type: "STRING",
-            autocomplete: true,
-            required: true,
-          },
-        ],
-      },
-      {
-        guildIds: registeredGuilds,
-        behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
-      }
-    );
-  }
+		return await interaction.respond(
+			// stationData.map((value, key) => {
+			//   return {
+			//     name: `📻 ${key}`,
+			//     value: key,
+			//   };
+			// })
+			[
+				{
+					name: `📻 Leaks`,
+					value: "nope",
+				},
+			]
+		);
+	}
+
+	public override registerApplicationCommands(
+		registry: ApplicationCommandRegistry
+	) {
+		registry.registerChatInputCommand(
+			{
+				name: this.name,
+				description: "Tune-in to a Juice WRLD radio station.",
+				options: [
+					{
+						name: "station",
+						description: "The station to tune-in to.",
+						type: "STRING",
+						autocomplete: true,
+						required: true,
+					},
+				],
+			},
+			{
+				guildIds: registeredGuilds,
+				behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
+			}
+		);
+	}
 }
--- src/commands/util/stats.ts
+++ src/commands/util/stats.ts
@@ -1,82 +1,82 @@
 import { EvieEmbed } from "#root/classes/EvieEmbed";
 import { registeredGuilds } from "#root/utils/parsers/envUtils";
 import {
-  ApplicationCommandRegistry,
-  Command,
-  RegisterBehavior,
+	ApplicationCommandRegistry,
+	Command,
+	RegisterBehavior,
 } from "@sapphire/framework";
 import {
-  CommandInteraction,
-  MessageActionRow,
-  MessageButton,
+	CommandInteraction,
+	MessageActionRow,
+	MessageButton,
 } from "discord.js";
 
 export class Stats extends Command {
-  public override async chatInputRun(interaction: CommandInteraction) {
-    interaction.reply({
-      embeds: [
-        new EvieEmbed()
-          .setTitle("Juice WRLD Stats")
-          .setDescription(
-            "Juice WRLD is part of the [Evie Discord Bot family](https://discord.gg/5MWQeVgP7f)."
-          )
-          .addField(
-            `__Shard Stats__`,
-            `
+	public override async chatInputRun(interaction: CommandInteraction) {
+		interaction.reply({
+			embeds: [
+				new EvieEmbed()
+					.setTitle("Juice WRLD Stats")
+					.setDescription(
+						"Juice WRLD is part of the [Evie Discord Bot family](https://discord.gg/5MWQeVgP7f)."
+					)
+					.addField(
+						`__Shard Stats__`,
+						`
             **Users**: ${interaction.client.guilds.cache.reduce(
-              (acc, guild) => acc + guild.memberCount,
-              0
-            )}
+				(acc, guild) => acc + guild.memberCount,
+				0
+			)}
             **Servers**: ${interaction.client.guilds.cache.reduce(
-              (acc) => acc + 1,
-              0
-            )}
+				(acc) => acc + 1,
+				0
+			)}
             **Streaming to**: ${interaction.client.voice.adapters.size} channels
           `,
-            true
-          )
-          .addField(
-            `__Radio Stats__`,
-            interaction.client.radio.radios
-              .map((_value, key) => {
-                return `
-                  **${key}** (${
-                  interaction.client.radio.queues.get(key)?.length ?? 0
-                } songs)
-                  Now Playing: **${
-                    interaction.client.radio.nowPlaying.get(key)?.title ??
-                    "Nothing"
-                  }**
-                  `;
-              })
-              .join("")
-          ),
-      ],
-      components: [
-        new MessageActionRow().addComponents(
-          new MessageButton()
-            .setURL(
-              "https://discord.com/api/oauth2/authorize?client_id=961142622636871781&permissions=3146752&scope=bot%20applications.commands"
-            )
-            .setLabel("Invite me to your server")
-            .setStyle("LINK")
-        ),
-      ],
-    });
-  }
+						true
+					),
+				// .addField(
+				//   `__Radio Stats__`,
+				//   interaction.client.radio.radios
+				//     .map((_value, key) => {
+				//       return `
+				//         **${key}** (${
+				//         interaction.client.radio.queues.get(key)?.length ?? 0
+				//       } songs)
+				//         Now Playing: **${
+				//           interaction.client.radio.nowPlaying.get(key)?.title ??
+				//           "Nothing"
+				//         }**
+				//         `;
+				//     })
+				//     .join("")
+				// ),
+			],
+			components: [
+				new MessageActionRow().addComponents(
+					new MessageButton()
+						.setURL(
+							"https://discord.com/api/oauth2/authorize?client_id=961142622636871781&permissions=3146752&scope=bot%20applications.commands"
+						)
+						.setLabel("Invite me to your server")
+						.setStyle("LINK")
+				),
+			],
+		});
+	}
 
-  public override registerApplicationCommands(
-    registry: ApplicationCommandRegistry
-  ) {
-    registry.registerChatInputCommand(
-      {
-        name: this.name,
-        description: "View stats and what's currently playing.",
-      },
-      {
-        guildIds: registeredGuilds,
-        behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
-      }
-    );
-  }
+	public override registerApplicationCommands(
+		registry: ApplicationCommandRegistry
+	) {
+		registry.registerChatInputCommand(
+			{
+				name: this.name,
+				description: "View stats and what's currently playing.",
+			},
+			{
+				guildIds: registeredGuilds,
+				behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
+			}
+		);
+	}
 }
